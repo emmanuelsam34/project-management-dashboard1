0c70801f3f6f566948280dd2511f66035f8f1a7c
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-02-17 15:56:34 +0100

    sign-in/sign-up page

--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -3,26 +3,44 @@ import { DottedSeparator } from '@/components/dotted-separator';
 import { Button } from '@/components/ui/button';
 import { FcGoogle } from 'react-icons/fc';
 import { FaGithub } from 'react-icons/fa';
+import  Link  from "next/link";
 
 import { 
     Card, 
-    CardContent, 
+    CardContent,
+    CardDescription, 
     CardHeader, 
     CardTitle } from '@/components/ui/card';
 
-export const SignInCard = () => {
+export const SignUpCard = () => {
     return (
         <Card className="w-full h-full md:w-[487px] border-none shadow-none">
             <CardHeader className="flex items-center justify-center text-center p-7">
                 <CardTitle className="text-2xl">
-                    Welcome Back
+                    Sign Up
                 </CardTitle>
+                <CardDescription className="text-sm">
+                    By signing up, you agree to our { " " }
+                    <Link href="/terms">
+                    <span className="text-orange-700">Terms of Service</span>
+                    </Link> and {""}
+                    <Link href="/privacy">
+                    <span className="text-blue-700">Policy</span></Link>   
+                </CardDescription>
             </CardHeader>
             <div className="px-7">
                 <DottedSeparator />
             </div>
             <CardContent className="p-7">
                 <form className="space-y-4">
+                <Input 
+                required
+                type="text"
+                value={""}
+                onChange={() => {}}
+                placeholder="Enter full name"
+                disabled={false}
+                />
                 <Input 
                 required
                 type="email"
