9e352832b9e793f5a70b14631f8fcf98abce62d8
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-02-22 09:34:27 +0100

    Logout middleware

--- features/auth/components/sign-up-card.tsx
+++ features/auth/components/sign-up-card.tsx
@@ -121,7 +121,7 @@ export const SignUpCard = () => {
             )}
             />
             <Button disabled={false} size="lg" className="w-full">
-                    Login 
+                    Sign Up
             </Button>
             </form>
             </Form>
