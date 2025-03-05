658f9d2fb352944a1b975db3a066b95881966e9d
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-03-05 16:20:25 +0100

    responsive modal changes

---
+++ components/responsive-modal.tsx
@@ -0,0 +1,21 @@
+import { useMedia } from "react-use";
+
+import {
+    Dialog,
+    DialogContent,
+} from "@/components/ui/dialog";
+
+import {
+    Drawer,
+    DrawerContent,
+} from "@/components/ui/drawer";
+
+interface ResponsiveModalProps {
+    children: React.ReactNode;
+    open: boolean;
+    onOpenChange: (open: boolean) => void;
+}
+
+export const ResponsiveModal = ({
+
+}: ResponsiveModalProps) => {}; 
