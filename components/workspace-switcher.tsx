72c43879be466b674dbbbd06ae6d4f594bdfed8e
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-03-06 10:41:49 +0100

    workspace switcher changes

--- components/workspace-switcher.tsx
+++ components/workspace-switcher.tsx
@@ -13,11 +13,14 @@ import {
 } from "@/components/ui/select";
 import { WorkspaceAvatarProps } from "@/features/workspaces/components/workspace-avatar";
 import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
+import { useCreateWorkspaceModal } from "@/features/workspaces/hooks/use-create-workspace-modal";
+
 
 export const WorkspaceSwitcher = () => {
     const workspaceId = useWorkspaceId();
     const router = useRouter();
     const { data: workspaces } = useGetWorkspaces();
+    const {open} = useCreateWorkspaceModal();
 
     const onSelect = (id: string) => {
         router.push(`/workspaces/${id}`);
@@ -27,7 +30,7 @@ export const WorkspaceSwitcher = () => {
         <div className="flex flex-col gap-y-2">
             <div className="flex items-center justify-between">
                 <p className="text-xs uppercase text-neutral-500">Workspaces</p>
-                <RiAddCircleFill 
+                <RiAddCircleFill onClick={open}
                     className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" 
                 />
             </div>
