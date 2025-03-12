5ee65042e9b785e1682526608648954617934b24
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-03-12 12:51:57 +0100

    route fixes

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,22 +1,20 @@
-import { Hono } from 'hono';
-import { handle } from 'hono/vercel';
+import { Hono } from "hono";
+import { handle } from "hono/vercel";
 
 import auth from "@/features/auth/server/route";
 import workspaces from "@/features/workspaces/server/route";
 import members from "@/features/members/server/route";
 
-const app = new Hono().basePath('/api');
+const app = new Hono().basePath("/api");
 
-app
-  .route("/auth", auth)
-  .route("/workspaces", workspaces)
-  .route("/members", members);
+app.route("/auth", auth);
+app.route("/workspaces", workspaces);
+app.route("/members", members);
 
 export const GET = handle(app);
 export const POST = handle(app);
-export const PATCH = handle(app);
 export const PUT = handle(app);
+export const PATCH = handle(app);
 export const DELETE = handle(app);
 
-export type AppType = typeof app;
 export default app;
