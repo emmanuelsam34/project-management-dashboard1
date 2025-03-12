a3c52192c9d5ecff7a9a67214271e64219fdfcf2
emmanuelsam34 <emmanuelagbedejobi@gmail.com>
2025-03-12 18:02:04 +0100

    projects page UI

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -4,7 +4,7 @@ import { handle } from 'hono/vercel';
 import auth from '@/features/auth/server/route';
 import workspaces from '@/features/workspaces/server/route';
 import members from '@/features/members/server/route';
-
+import projects from '@/features/projects/server/route';
 
 const app = new Hono().basePath('/api');
 
@@ -12,6 +12,7 @@ const app = new Hono().basePath('/api');
 app.route('/auth', auth);
 app.route('/workspaces', workspaces);
 app.route('/members', members);
+app.route('/projects', projects);
 
 
 export const GET = handle(app);
--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -3,6 +3,7 @@ import Link from 'next/link';
 import { DottedSeparator } from './dotted-separator';
 import { Navigation } from './navigation';
 import { WorkspaceSwitcher } from './workspace-switcher';
+import { Projects } from './projects';
 
 
 export const Sidebar = () => {
@@ -14,6 +15,8 @@ export const Sidebar = () => {
             <DottedSeparator className='my-4'/>
             <WorkspaceSwitcher />
             <Navigation />
+            <DottedSeparator className='my-4'/>
+            <Projects />
         </aside>
     );
 }
